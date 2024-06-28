225f5eec624814e18e2938d73a43dcc9ca3016dd
SergeyFedukov <freedomsfed@gmail.com>
2024-06-28 15:57:07 +0700

    Added abillity to create a new hive if bee capacity lower than count of bees

--- Assets/Scripts/Hive.cs
+++ Assets/Scripts/Hive.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 public class Hive : MonoBehaviour, IStateObject
 { 
     [SerializeField] private BeeWorkerView _prefubBeeWorkerView;
+    private int _beeCapacity;
     private List<BeeWorker> _bees = new List<BeeWorker>();
-    private BeeQueen _beeQueen = new BeeQueen(3f, 2f);
+    private BeeQueen _beeQueen = new BeeQueen(30f, 2f);
+
+    private void Awake()
+    {
+        _beeCapacity = 1;
+    }
 
     public void ChangeState(Cell topCell, Cell bottomCell, Cell leftCell, Cell rightCell)
     {
         _beeQueen?.ChangeState();
+        if (_bees.Count < _beeCapacity)
+        {
+            BeeWorker beeWorker = _beeQueen?.TryBreedBee(_prefubBeeWorkerView, transform.position + new Vector3(0, 0.2f, -0.6f), 60f, 1f, 2, 1f);
+            if (beeWorker != null)
+                _bees.Add(beeWorker);
+        }
+        else
+        {
+            Cell[] cells = { topCell, bottomCell, leftCell, rightCell };
 
-        BeeWorker beeWorker = _beeQueen?.TryBreedBee(_prefubBeeWorkerView, transform.position + new Vector3(0, 0.2f, -0.6f), 60f, 1f, 2, 1f);
-        if (beeWorker != null)
-            _bees.Add(beeWorker);
+            foreach (var cell in cells)
+                if (cell != null && cell.TryTakeStateObject(this))
+                    break;
+        }
 
         foreach (var bee in _bees)
             bee.ChangeState();
-
+            
     }
 }
